c1ff8e74677e30f27de68c6f6a2eac9019bd22f3
Aman Kumar Sinha <amankrsinha07@gmail.com>
2023-12-14 19:40:12 +0530

    chore:added local storage handling for taskList

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import TaskInputForm from "./components/TaskInputForm";
 import Todos from "./components/Todos";
@@ -7,9 +7,26 @@ import Footer from "./components/Footer";
 
 function App() {
   const [task, setTask] = useState("");
-  const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState(null); // Set initial state to null
   const [inputError, setInputError] = useState(false);
   const [showPopup, setShowPopup] = useState(false);
+
+  // Load tasks from localStorage on component mount
+  useEffect(() => {
+    const storedTaskList = JSON.parse(localStorage.getItem("taskList")) || [];
+
+    // Set the taskList state only if it's null
+    if (taskList === null) {
+      setTaskList(storedTaskList);
+    }
+  }, [taskList]);
+
+  // Save tasks to localStorage whenever taskList changes
+  useEffect(() => {
+    if (taskList !== null) {
+      localStorage.setItem("taskList", JSON.stringify(taskList));
+    }
+  }, [taskList]);
 
   function handleClickTaskAdd() {
     const formattedTask = task.trim().toLowerCase();
@@ -36,7 +53,9 @@ function App() {
       return;
     }
 
-    setTaskList([...taskList, { text: task, isChecked: false }]);
+    // Update taskList state and localStorage
+    const updatedTaskList = [...(taskList || []), { text: task, isChecked: false }];
+    setTaskList(updatedTaskList);
     setTask("");
   }
 
@@ -56,7 +75,7 @@ function App() {
           handleClickTaskAdd={handleClickTaskAdd}
         />
 
-        <Todos taskList={taskList} setTaskList={setTaskList} />
+        <Todos taskList={taskList || []} setTaskList={setTaskList} />
 
         {showPopup && (
           <div className="popup">
